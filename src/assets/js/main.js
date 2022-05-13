92d68b6d336d6691c1f8ba4d51fb2066f7c8cce0
Tom Checkley <tomc@element78.co.uk>
2022-05-13 11:06:18 +0100

    FormControl js & firebase config files

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { firebaseConfig } from "../../../config/firebase.config.js";
+import { ContactFormComponent } from "./Components/Form/ContactFormComponent";
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-=======
-import "svgxuse";
-import "../scss/main.scss";
-
-import formComponent from "./components/form.js";
-
-formComponent.init();
->>>>>>> main
+const form = document.getElementById("contactForm");
+const contactForm = new ContactFormComponent(form);
+contactForm.init();
