8b79550d774f52e1ee6cca3627c762744fd161ac
Tom Checkley <tomc@element78.co.uk>
2022-05-14 08:11:49 +0100

    added checks for if modal modal exists

--- src/assets/js/Components/Form/ContactFormComponent.js
+++ src/assets/js/Components/Form/ContactFormComponent.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection, doc, addDoc } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 import { config } from "../../../../../config/site-config";
-
-import { getFirestore, collection, doc, addDoc } from "firebase/firestore";
+import { Modal } from "../Modal";
 import { FormControl } from "./FormControl";
 
 export class ContactFormComponent {
 
     constructor(el) {
-        this.el = el;
+        this.contactForm = el;
         this.app = initializeApp(config.firebase);
         this.db = getFirestore(this.app);
         this.analytics = getAnalytics(this.app);
@@ -18,22 +18,27 @@ export class ContactFormComponent {
 
     init() {
         // this.addRecaptcha();
-        const formControlElements = [...this.el.querySelectorAll("[data-form-control]")];
+        const formControlElements = [...this.contactForm.querySelectorAll("[data-form-control]")];
         formControlElements.forEach(el => {
             const formControl = new FormControl(el);
             formControl.init();
             this.formControls.push(formControl);
         });
 
-        this.el.addEventListener("submit", (e) => {
+        this.contactForm.addEventListener("submit", (e) => {
             this.submit(e);
         })
+
+        const modalEl = this.contactForm.querySelector("[data-modal]");
+        if (modalEl) {
+            this.modal = new Modal(modalEl);
+        }
     }
 
     async submit(e) {
         e.preventDefault();
 
-        const formData = new FormData(this.el);
+        const formData = new FormData(this.contactForm);
 
         const date = new Date();
         const posted = {
@@ -58,7 +63,7 @@ export class ContactFormComponent {
         try {            
             const docRef = await addDoc(collection(this.db, "messages"), newMessage)
                 .then(res => {                
-                    this.el.reset();
+                    this.contactForm.reset();
                     console.log(this.formControls);
                     
                     this.formControls.forEach(formControl => {
@@ -67,13 +72,9 @@ export class ContactFormComponent {
                         formControl.el.classList.remove("opened");
                     });
 
-                    const modal = this.el.querySelector("[data-modal]");
-                    modal.removeAttribute("hidden");
-                    modal.focus();
-                    var closeButton = modal.querySelector("button");
-                    closeButton.addEventListener("click", () => {
-                        modal.setAttribute("hidden", "");
-                    })
+                    if (this.modal) {
+                        this.modal.open();
+                    }
 
                 });
 
