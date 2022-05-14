4f1fecaccdd5bb11f900bc1986cc606111bd138b
Tom Checkley <tomc@element78.co.uk>
2022-05-14 12:27:48 +0100

    years experience

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,3 +4,13 @@ import { ContactFormComponent } from "./Components/Form/ContactFormComponent";
 const form = document.getElementById("contactForm");
 const contactForm = new ContactFormComponent(form);
 contactForm.init();
+
+(function() {
+    const yearNow = new Date().getFullYear();
+    const yearsExp = yearNow - 2013;
+    
+    const yearsExpHolder = document.querySelector("[data-years-exp");
+    if (yearsExpHolder) {
+        yearsExpHolder.innerHTML = yearsExp;
+    }
+}());
